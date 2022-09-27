ccc63212cb8e8dc87d68009d17e1267a6e74531e
Di0n4th4 <dionathadmarchi@gmail.com>
2022-09-27 20:16:28 -0300

    adicionando funcao 2

---
+++ funcoes/Func02.sql
@@ -0,0 +1,17 @@
+CREATE FUNCTION return_reserva_assento(in_reserva INTEGER) RETURNS numeric AS $$
+DECLARE
+var_reserva_assento NUMERIC;
+BEGIN
+SELECT count(reserva.cadeira) INTO var_reserva_assento FROM reserva
+INNER JOIN sessao
+ON reserva.cod_sessao = sessao.cod_sessao
+INNER JOIN pedido
+ON reserva.cod_pedido = pedido.cod_pedido
+WHERE reserva.cod_reserva = in_reserva;
+if(var_reserva_assento > 0) then
+	RETURN var_reserva_assento;
+else
+	RAISE EXCEPTION 'Este assento está disponível para reserva.';
+END if;		
+END; $$
+LANGUAGE PLPGSQL;
