5da8e25bc4a1340445fa0e96cd6c575f8d629a5e
Dionatha De Marchi <dionatha@coasa.br>
2022-09-27 20:09:16 -0300

    Adicionando a funcao 1

---
+++ funcoes/Func01.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION return_ingressos_disponiveis(in_sessao INTEGER) RETURNS numeric AS $$
+DECLARE
+var_ingressos_disponiveis NUMERIC;
+BEGIN
+SELECT ingressos_disponiveis into var_ingressos_disponiveis
+FROM sessao 
+WHERE cod_sessao = in_sessao;
+if(var_ingressos_disponiveis > 0) then
+	RETURN var_ingressos_disponiveis;
+else
+	RAISE EXCEPTION 'Não há mais ingressos disponíveis para essa Sessão.';	
+END if;
+END; $$
+LANGUAGE PLPGSQL;
